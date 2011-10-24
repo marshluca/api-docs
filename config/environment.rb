8389dbaa62ffaeec14523c1c12410cd9074a3691
Lin Zhang <marshluca@gmail.com>
2011-10-24 10:06:27 +0800

    Enable vi editing mode for console

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Enable vi editing mode for console
+if RUBY_PLATFORM =~ /darwin/i
+  Readline.try(:vi_editing_mode) rescue nil
+end
+
 # Initialize the rails application
 Wiki::Application.initialize!
