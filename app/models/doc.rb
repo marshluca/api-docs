927cf7ab45e3a89dae1d0bbe96776e63267960b8
Lin Zhang <marshluca@gmail.com>
2011-10-26 01:29:28 +0800

    define the fields of doc model

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,3 +1,16 @@
 class Doc
   include Mongoid::Document
+
+  field :version
+  field :section
+  field :name
+  field :path
+  field :url
+  field :format
+  field :request
+  field :auth
+  field :access
+  field :parameters
+  field :response
+  field :annotation
 end
