e5312fb43f7156f740aa45e6807b31095b7a3372
Lin Zhang <marshluca@gmail.com>
2011-10-26 16:44:50 +0800

    add fields for doc model

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,18 +1,19 @@
 class Doc
   include Mongoid::Document
 
-  field :project
-  field :version
-  field :section
-  field :name
-  field :path
-  field :url
-  field :format
-  field :auth
+  field :project  # 项目
+  field :version  # 版本
+  field :category # 分类
+  field :name     # 名称
+  field :path     # 调用path
+  field :url      # 请求url
+  field :desc     # 接口描述
+  field :format   # 支持格式[json | xml]
+  field :auth     # 访问权限
   field :access
-  field :parameters
+  field :parameters # 请求参数
   field :request
-  field :response
-  field :annotation
+  field :response # 返回内容
+  field :annotation # 说明
 
 end
