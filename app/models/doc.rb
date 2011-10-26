9b677379b13326eb2a47eb22e3f317fcfcf42267
Lin Zhang <marshluca@gmail.com>
2011-10-26 17:54:29 +0800

    add fields for app/model/doc.rb

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -8,12 +8,14 @@ class Doc
   field :path     # 调用path
   field :url      # 请求url
   field :desc     # 接口描述
-  field :format   # 支持格式[json | xml]
+  field :format   # 支持格式 [json | xml]
   field :auth     # 访问权限
-  field :access
-  field :parameters # 请求参数
-  field :request
-  field :response # 返回内容
-  field :annotation # 说明
+  field :parameters  # 请求参数
+  field :http_method # HTTP请求类型
+  field :sample      # 返回示例, 嵌入gist
+  field :annotation  # 返回字段说明
+  field :deprecated   # 是否弃用
+  field :updated_at  # 更新时间
+  field :created_at  # 创建时间
 
 end
