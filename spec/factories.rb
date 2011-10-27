ab5317bd1bd86bdef428f9113c119eb68dd80dd1
Lin Zhang <marshluca@gmail.com>
2011-10-27 14:31:59 +0800

    facotory the fixtures for other models

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,7 +6,16 @@ Factory.define :user do |u|
   u.password 'please'
 end
 
+Factory.define :project do |p|
+  p.name 'Test Project'
+  p.desc 'a test project'
+end
+
 Factory.define :doc do |d|
   d.name 'Test API'
   d.desc 'a test api'
+end
+
+Factory.define :user_log do |log|
+  #...
 end
