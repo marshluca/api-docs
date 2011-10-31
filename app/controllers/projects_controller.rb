8a42bfc3f210d1358eb032c5d036aa749455a223
Lin Zhang <marshluca@gmail.com>
2011-10-31 15:21:13 +0800

    add author hash to params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,13 @@
 class ProjectsController < BaseController
   inherit_resources
   actions :all
+
+  def create
+    params[:project].merge!(:author => {
+      :id    => current_user.id,
+      :name  => current_user.name,
+      :email => current_user.email
+    })
+    create!
+  end
 end
