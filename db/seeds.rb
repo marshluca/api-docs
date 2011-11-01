33b76d24112a2ed169cc4c616e8d77d6fb199143
Lin Zhang <marshluca@gmail.com>
2011-11-01 10:21:09 +0800

    add test data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,23 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-puts 'EMPTY THE MONGODB DATABASE'
-Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'Admin', :email => 'admin@example.com', :password => '123456', :password_confirmation => '123456'
+
+# puts 'EMPTY THE MONGODB DATABASE'
+# Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
+# puts 'SETTING UP DEFAULT USER LOGIN'
+
+user = User.create! :name => 'admin', :email => 'admin@gmail.com', :password => 'secret', :password_confirmation => 'secret'
 puts 'New user created: ' << user.name
+
+project = Project.create! :name => 'share', :cname => '易分享', :version => 'v1', :desc => 'www.yfx365.com', :author => Author.new(:name => user.name, :email => user.email), :categories => [Category.new(:name => '壁纸'), Category.new(:name => '铃声'), Category.new(:name => '笑话'), Category.new(:name => '短信'), Category.new(:name => '应用'), Category.new(:name => '游戏'), Category.new(:name => '动态'), Category.new(:name => '达人'), Category.new(:name => '用户'), Category.new(:name => '搜索')]
+puts 'New project created: ' << project.name
+
+%w(wallpapers/recommend wallpapers/popular wallpapers/recent wallpapers/mine ringtones/recommend ringtones/popular ringtones/recent ringtones/mine).each do |i|
+  Doc.create! :name => i, :format => 'json, xml', :http_method => 'Get', :project_id => project.id, :desc => '获取壁纸列表数据', :url => "http://ws.yfx365.com/v1/#{i}", :author => project.author, :category => project.categories.first
+  puts
+end
+
+%w(ringtones/recommend ringtones/popular ringtones/recent ringtones/mine).each do |i|
+  d = Doc.create! :name => i, :format => 'json, xml', :http_method => 'Get', :project_id => project.id, :desc => '获取铃声列表数据', :url => "http://ws.yfx365.com/v1/#{i}", :author => project.author, :category => project.categories[1]
+  puts 'New document created: ' << d.name
+end
