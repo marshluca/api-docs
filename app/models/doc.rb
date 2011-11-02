0f04b4be6d5c96de39cb3bc80b7a8fac5e6df3ea
Lin Zhang <marshluca@gmail.com>
2011-11-02 11:53:17 +0800

    add column to admin table list

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -4,22 +4,22 @@ class Doc
 
   # cache
 
-  field :name, :type => String                     # 名称
-  field :desc, :type => String                     # 接口描述
-  field :url, :type => String                      # 请求url
-  field :format, :type => String                   # 支持格式 [json|xml]
-  field :http_method, :type => String              # HTTP请求类型
-  field :gist_url, :type => String                 # 返回示例 (嵌入gist)
-  field :annotation, :type => String               # 返回字段说明
-  field :deprecated, :type => Boolean              # 是否弃用
-  field :authenticated, :type => Boolean           # 是否需要登录认证 (默认不需要)
-  field :kind, :type => Integer, :default => 0     # 类型 [doc]
-  field :parameters, :type => Hash, :default => {} # 请求参数
+  field :name, :type => String                               # 名称
+  field :desc, :type => String                               # 接口描述
+  field :url, :type => String                                # 请求url
+  field :format, :type => String                             # 支持格式 [json|xml]
+  field :http_method, :type => String                        # HTTP请求类型
+  field :gist_url, :type => String                           # 返回示例 (嵌入gist)
+  field :annotation, :type => String                         # 返回字段说明
+  field :kind, :type => Integer, :default => 0               # 类型 [doc]
+  field :parameters, :type => Hash, :default => {}           # 请求参数
+  field :deprecated, :type => Boolean, :default => false     # 是否弃用
+  field :authenticated, :type => Boolean, :default => false  # 是否需要登录认证 (默认不需要)
 
-  embeds_one :author                               # 创建作者
-  embeds_one :category                             # 分类
-  references_many :user_logs                       # 关联日志
-  referenced_in :project, :index => true           # 关联项目
+  embeds_one :author                                         # 创建作者
+  embeds_one :category                                       # 分类
+  references_many :user_logs                                 # 关联日志
+  referenced_in :project, :index => true                     # 关联项目
 
   index [[:name, Mongo::ASCENDING]], :background => true
   index 'category.name'
