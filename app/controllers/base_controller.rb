e25e44a7417cd372fd9708822ec15fad69518983
Lin Zhang <marshluca@gmail.com>
2011-11-03 17:37:30 +0800

    remove set_default_project

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,9 +1,3 @@
 class BaseController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :set_default_project
-
-  private
-  def set_default_project
-    @project = Project.where(:name => current_user.projects.last).first
-  end
 end
