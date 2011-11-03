7c100308d53d25d7b4fb75afb91136002cf9018d
Lin Zhang <marshluca@gmail.com>
2011-11-03 12:03:25 +0800

    display user log in user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < BaseController
   inherit_resources
   actions :index, :show
+
+  def show
+    @logs = UserLog.where('author.name' => current_user.name)
+    show!
+  end
 end
