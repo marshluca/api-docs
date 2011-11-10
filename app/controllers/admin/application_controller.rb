9d619a784370bdced99cd08d2e5c893c2185604e
Lin Zhang <marshluca@gmail.com>
2011-11-10 20:30:02 +0800

    rename method: authorize_admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,10 +2,10 @@ class Admin::ApplicationController < ApplicationController
   layout 'admin'
 
   before_filter :authenticate_user!
-  before_filter :authenticate_admin!
+  before_filter :authorize_admin!
 
   private
-    def authenticate_admin!
+    def authorize_admin!
       unless current_user.admin?
         redirect_to root_url, :alert => 'Access Denied.'
       end
