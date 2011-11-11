cce13c44771364718c89771e0b2ed524bf582598
Lin Zhang <marshluca@gmail.com>
2011-11-11 17:46:29 +0800

    fixed the delete action in admin

--- app/controllers/admin/docs_controller.rb
+++ app/controllers/admin/docs_controller.rb
@@ -46,7 +46,7 @@ class Admin::DocsController < Admin::ApplicationController
 
     respond_to do |format|
       if @doc.save
-        format.html { redirect_to project_doc_url(@project, @doc), :notice => 'Doc was successfully created.' }
+        format.html { redirect_to admin_project_docs_url(@project), :notice => 'Doc was successfully created.' }
         format.json { render :json => @doc, :status => :created, :location => @doc }
       else
         format.html { render :action => "new" }
@@ -62,7 +62,7 @@ class Admin::DocsController < Admin::ApplicationController
 
     respond_to do |format|
       if @doc.update_attributes(params[:doc])
-        format.html { redirect_to project_doc_url(@project, @doc), :notice => 'Doc was successfully updated.' }
+        format.html { redirect_to admin_project_docs_url(@project), :notice => 'Doc was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render :action => "edit" }
@@ -75,10 +75,10 @@ class Admin::DocsController < Admin::ApplicationController
   # DELETE /admin/docs/1.json
   def destroy
     @doc = @project.docs.find(params[:id])
-    @project.docs.destroy
+    @doc.destroy
 
     respond_to do |format|
-      format.html { redirect_to admin_docs_url }
+      format.html { redirect_to admin_project_docs_url(@project) }
       format.json { head :ok }
     end
   end
