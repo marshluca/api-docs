54099ba4117a9e72a8ee0c132aad2d82bd9d8ea2
Lin Zhang <marshluca@gmail.com>
2011-12-03 15:50:53 +0800

    disable the quiet asset logs for development

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,9 @@ if RUBY_PLATFORM =~ /darwin/i
   Readline.try(:vi_editing_mode) rescue nil
 end
 
+# Specify the logger
+# Rails.logger = Logger.new(STDOUT)
+# Rails.logger = Log4r::Logger.new("Application Log")
+
 # Initialize the rails application
 Wiki::Application.initialize!
