66da31bfa58601a37c1ee47435e8a86dd53e26ff
Lin Zhang <marshluca@gmail.com>
2011-12-05 18:57:39 +0800

    add albino and nokogiri gems to parse and colorize the annotation of doc

--- app/helpers/docs_helper.rb
+++ app/helpers/docs_helper.rb
@@ -5,11 +5,11 @@ module DocsHelper
   end
 
   def syntax_highlighter(html)
-    html
-    # doc = Nokogiri::HTML(html)
-    # doc.search("//pre[@lang]").each do |pre|
-    #   pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
-    # end
-    # doc.to_s
+    # html
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
   end
 end
