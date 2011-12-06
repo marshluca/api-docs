a25c42f8b9598a1c99bc013a17b08c7e5d6d4847
Lin Zhang <marshluca@gmail.com>
2011-12-06 10:34:38 +0800

    redefine markdown method

--- app/helpers/docs_helper.rb
+++ app/helpers/docs_helper.rb
@@ -1,11 +1,10 @@
 module DocsHelper
   def markdown(text)
-    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-    syntax_highlighter(Redcarpet.new(text.to_s, *options).to_html).html_safe
+    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :tables => true)
+    syntax_highlighter(md.render(text.to_s)).html_safe
   end
 
   def syntax_highlighter(html)
-    # html
     doc = Nokogiri::HTML(html)
     doc.search("//pre[@lang]").each do |pre|
       pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
