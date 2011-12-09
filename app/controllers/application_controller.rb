1a4e00e623d67c29b0c33ab516f9e31eae08f37c
Lin Zhang <marshluca@gmail.com>
2011-12-09 11:00:23 +0800

    comment included module: FrontendHelpers::Html5Helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include FrontendHelpers::Html5Helper
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
